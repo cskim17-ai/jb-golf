e3156e88a16151cb70acf9ec753b55e77d955fea
cskim17-ai <cskim1747@gmail.com>
2026-04-04 21:36:19 +0900

    Refactor main.tsx to simplify rendering logic
    Removed redirect logic for '/jb-golf/' and uncommented rendering of the App component.

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,19 @@ import App from './App.tsx';
 import './index.css';
 
 // 리다이렉트 로직 추가
-if (window.location.pathname.includes('/jb-golf/')) {
-  window.location.href = "https://cskim17-ai.github.io";
-} else {
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-}
+//if (window.location.pathname.includes('/jb-golf/')) {
+//  window.location.href = "https://cskim17-ai.github.io";
+//} else {
+//  createRoot(document.getElementById('root')!).render(
+//    <StrictMode>
+//      <App />
+//    </StrictMode>,
+//  );
+//}
 
 
-//createRoot(document.getElementById('root')!).render(
-//  <StrictMode>
-//    <App />
-//  </StrictMode>,
-//);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
