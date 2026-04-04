22ae9626589ea05e26b9af05b68c52d1864e2931
cskim17-ai <cskim1747@gmail.com>
2026-04-04 21:35:18 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 
 // 리다이렉트 로직 추가
 if (window.location.pathname.includes('/jb-golf/')) {
-  window.location.href = "https://cskim17-ai.github.io/jb-golf";
+  window.location.href = "https://cskim17-ai.github.io";
 } else {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
